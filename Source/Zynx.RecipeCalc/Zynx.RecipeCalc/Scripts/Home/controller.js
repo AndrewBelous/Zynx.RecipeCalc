00654d226e23f8392a3552cde9b7d2048538d776
abelous1979 <andrew@docstoc.com>
2013-10-06 12:41:34 -0700

    Angular controller for home page
    Wired up html template to controller js
    Displays simple list of recipe names

--- Source/Zynx.RecipeCalc/Zynx.RecipeCalc/Scripts/Home/controller.js
+++ Source/Zynx.RecipeCalc/Zynx.RecipeCalc/Scripts/Home/controller.js
@@ -1,4 +1,26 @@
-﻿$(function()
+﻿//$(function()
+//{
+//	console.log("Controller loaded");
+//});
+
+function RecipeListCtrl($scope)
 {
-	console.log("Controller loaded");
-});
+	$scope.recipies = [
+	  {
+	  	"Name": "Recipe 1",
+	  	
+	  },
+	  {
+	  	"Name": "Recipe 2",
+	  	
+	  },
+	  {
+	  	"Name": "Recipe 3",
+	  	
+	  }
+	];
+}
+
+// angular bindings for app and controllers
+var recipeCalcApp = angular.module('recipeCalcApp', []);
+recipeCalcApp.controller('RecipeListCtrl', RecipeListCtrl);
